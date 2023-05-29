3038f61b26ea602bf3d2ff425b6ba5c49566c2aa
RaulLoyola <raul.loy@gmail.com>
2023-05-29 11:44:47 -0600

    Adding Google API

--- client/src/components/Table/GCampaigns.jsx
+++ client/src/components/Table/GCampaigns.jsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { TableFooter } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import { statusStyle } from './utils';
 import { googleCampaignsData } from '../../data/google';
+import DateRangeInput from '../DatePickers/DateRangeInput';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { contactsData } from '../../data/hubspot';
+import { Modal, Button } from 'react-bootstrap';
+import { facilitadores } from '../../facilitadores';
 import './Table.css';
 
 export default function GoogleCampaignsTable() {
-  const [since, setSince] = useState('2023-04-01');
-  const [until, setUntil] = useState('2023-04-15');
+  const [since, setSince] = useState(
+    localStorage.getItem('since') || '2023-04-15'
+  );
+  const [until, setUntil] = useState(
+    localStorage.getItem('until') || '2023-04-30'
+  );
 
   const [googleCampaignInsights, setGoogleCampaignInsights] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,6 +26,10 @@ export default function GoogleCampaignsTable() {
         const campaignsResponse = await googleCampaignsData(since, until);
 
         setGoogleCampaignInsights(campaignsResponse);
+
+        // Fetch Contacts
+        const contactsResponse = await contactsData(since, until);
+        setContacts(contactsResponse);
       } catch (err) {
         console.log(err);
       }
@@ -32,191 +37,186 @@ export default function GoogleCampaignsTable() {
     fetchData();
   }, [since, until]);
 
-  console.log(googleCampaignInsights);
+  const googleContacts = contacts.filter((element) =>
+    element.properties.hs_analytics_first_url.includes('ads.google.com')
+  );
+
+  const contactsbyCampaign = googleContacts.map(({ id, properties }) => ({
+    id,
+    hs_analytics_first_url: properties.hs_analytics_first_url
+      ? properties.hs_analytics_first_url.match(/hsa_cam=(\d+)/)?.[1]
+      : null,
+    lifecyclestage: properties.lifecyclestage,
+  }));
+
+  const contactCountsByCampaign = googleContacts.reduce((acc, contact) => {
+    const campaignId =
+      contact.properties.hs_analytics_first_url?.match(/hsa_cam=(\d+)/)?.[1] ||
+      null;
+    acc[campaignId] = (acc[campaignId] || 0) + 1;
+    return acc;
+  }, {});
+
+  const [show, setShow] = useState(false);
+  const [contactsInfo, setContactsInfo] = useState([]);
+  const [modalTitle, setModalTitle] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = (campaignID, filter) => {
+    const matchingCampaign = contactsbyCampaign.filter(
+      (campaign) => campaign.hs_analytics_first_url === campaignID.toString()
+    );
+    const matchingContact = googleContacts.filter((contact) =>
+      matchingCampaign.some(
+        (campaign) =>
+          campaign.id === contact.id &&
+          (filter ? filter === contact.properties.lifecyclestage : true)
+      )
+    );
+    setShow(true);
+    setContactsInfo(matchingContact);
+
+    const modalTitle = filter ? `Asignaciones - ${filter}` : 'Asignaciones';
+    setModalTitle(modalTitle);
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 120 },
+    { field: 'campaign', headerName: 'Campaña', width: 400 },
+    { field: 'campaignType', headerName: 'Tipo de campaña', width: 160 },
+    { field: 'spent', headerName: 'Gastado', width: 140 },
+    {
+      field: 'assignments',
+      headerName: 'Asignaciones',
+      width: 200,
+      headerAlign: 'center',
+      align: 'center',
+      renderCell: (params) => (
+        <Button
+          variant="link"
+          className="Details"
+          onClick={() => handleShow(params.row.id)}
+        >
+          {contactCountsByCampaign[params.row.id] || 0}
+        </Button>
+      ),
+    },
+    { field: 'impressions', headerName: 'Impresiones', width: 120 },
+    { field: 'clicks', headerName: 'Clics', width: 100 },
+    { field: 'cpc', headerName: 'CPC', width: 100 },
+    { field: 'ctr', headerName: 'CTR', width: 100 },
+  ];
+
+  const rows = googleCampaignInsights.map((row) => ({
+    id: row.campaign.id,
+    campaign: row.campaign.name,
+    campaignType:
+      row.campaign.advertising_channel_type === 12
+        ? 'Discovery'
+        : row.campaign.advertising_channel_type === 2
+        ? 'Search'
+        : row.campaign.advertising_channel_type === 3
+        ? 'Display'
+        : row.campaign.advertising_channel_type === 6
+        ? 'Video'
+        : row.campaign.advertising_channel_type,
+    spent: `$${(parseFloat(row.metrics.cost_micros) / 1000000).toLocaleString(
+      'en-US'
+    )}`,
+    assignments: [contactCountsByCampaign].reduce(
+      (acc, obj) => (row.campaign.id in obj ? obj[row.campaign.id] : acc),
+      0
+    ),
+    impressions: parseInt(row.metrics.impressions).toLocaleString('en-US'),
+    clicks: parseInt(row.metrics.clicks).toLocaleString('en-US'),
+    cpc: `$${(parseFloat(row.metrics.average_cpc) / 1000000).toFixed(2)}`,
+    ctr: `${(parseFloat(row.metrics.ctr) * 100).toFixed(2)} %`,
+  }));
 
   return (
-    <div className="Table">
-      <h3>Campaign Insights</h3>
+    <div>
+      <div className="Table">
+        <DateRangeInput
+          since={since}
+          setSince={setSince}
+          until={until}
+          setUntil={setUntil}
+        />
 
-      <TableContainer
-        component={Paper}
-        style={{
-          boxShadow: '0px 13px 20px 0px #80808029',
-          overflow: 'auto',
-          backgroundColor: 'transparent',
-        }}
-        sx={{ maxHeight: 350 }}
-      >
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Campaña</TableCell>
-              <TableCell align="center">Objetivo</TableCell>
-              <TableCell align="left">Gastado</TableCell>
-              <TableCell align="left">Resultados</TableCell>
-              <TableCell align="left">Costo</TableCell>
-              <TableCell align="center">Estado</TableCell>
-              <TableCell align="left">Asignaciones</TableCell>
-              <TableCell align="left">Alcance</TableCell>
-              <TableCell align="left">Impresiones</TableCell>
-              <TableCell align="left">Clics</TableCell>
-              <TableCell align="left">CPC</TableCell>
-              <TableCell align="left">CTR</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody style={{ color: 'white' }}>
-            {googleCampaignInsights.map((row) => (
-              <TableRow
-                key={row.campaign.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.campaign.name}
-                </TableCell>
-                <TableCell align="center">
-                  {row.metrics.all_conversions}
-                </TableCell>
-                <TableCell align="center">
-                  $
-                  {parseFloat(
-                    row.insights ? row.insights.data[0].spend : 0
-                  ).toLocaleString('en-US')}
-                </TableCell>
-                <TableCell align="center">
-                  {row.objective === 'MESSAGES' &&
-                  row.insights &&
-                  row.insights.data &&
-                  row.insights.data[0].actions
-                    ? (
-                        row.insights.data[0].actions.find(
+        <div className="table-container ">
+          <DataGrid
+            rows={[...rows]}
+            columns={columns}
+            checkboxSelection
+            components={{ Toolbar: GridToolbar }}
+          />
+        </div>
+
+        <Modal show={show} onHide={handleClose} className="fullscreen-modal">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {modalTitle} ({contactsInfo.length})
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div style={{ overflowX: 'auto' }}>
+              <table className="popup-table">
+                <thead>
+                  <tr>
+                    <th>Desarrollo</th>
+                    <th>Canal de captación</th>
+                    <th>Subcanal de captación</th>
+                    <th>Fecha de asignación</th>
+                    <th>Correo</th>
+                    <th>Fecha de creación</th>
+                    <th>Facilitador</th>
+                    <th>Fuente original</th>
+                    <th>Etapa del ciclo de vida</th>
+                    <th>Estado del lead</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {contactsInfo.map((contact) => (
+                    <tr key={contact.id}>
+                      <td>{contact.properties.desarrollo}</td>
+                      <td>{contact.properties.canal_de_captacion}</td>
+                      <td>{contact.properties.sub_canal_de_captacion}</td>
+                      <td>
+                        {new Date(
+                          contact.properties.hubspot_owner_assigneddate
+                        ).toLocaleDateString('es-MX')}
+                      </td>
+                      <td>{contact.properties.email}</td>
+                      <td>
+                        {new Date(
+                          contact.properties.createdate
+                        ).toLocaleDateString('es-MX')}
+                      </td>
+                      <td>
+                        {facilitadores.find(
                           (element) =>
-                            element.action_type ===
-                            'onsite_conversion.messaging_conversation_started_7d'
-                        ) || {}
-                      ).value + ' Msgs'
-                    : row.objective === 'OUTCOME_ENGAGEMENT' &&
-                      row.insights &&
-                      row.insights.data &&
-                      row.insights.data[0].actions
-                    ? (
-                        row.insights.data[0].actions.find(
-                          (element) => element.action_type === 'like'
-                        ) || {}
-                      ).value + ' Likes'
-                    : (row.objective === 'OUTCOME_LEADS' ||
-                        row.objective === 'LEAD_GENERATION') &&
-                      row.insights &&
-                      row.insights.data &&
-                      row.insights.data[0].actions
-                    ? (
-                        row.insights.data[0].actions.find(
-                          (element) => element.action_type === 'lead'
-                        ) || {}
-                      ).value + ' Leads'
-                    : (row.objective === 'LINK_CLICKS' ||
-                        row.objective === 'OUTCOME_TRAFFIC') &&
-                      row.insights &&
-                      row.insights.data &&
-                      row.insights.data[0].actions
-                    ? (
-                        row.insights.data[0].actions.find(
-                          (element) => element.action_type === 'link_click'
-                        ) || {}
-                      ).value + ' Clicks'
-                    : 0}
-                </TableCell>
-                <TableCell align="center">
-                  $
-                  {(
-                    (row.insights ? row.insights.data[0].spend : 0) /
-                    parseFloat(
-                      row.objective === 'MESSAGES' &&
-                        row.insights &&
-                        row.insights.data &&
-                        row.insights.data[0].actions
-                        ? (
-                            row.insights.data[0].actions.find(
-                              (element) =>
-                                element.action_type ===
-                                'onsite_conversion.messaging_conversation_started_7d'
-                            ) || {}
-                          ).value
-                        : row.objective === 'OUTCOME_ENGAGEMENT' &&
-                          row.insights &&
-                          row.insights.data &&
-                          row.insights.data[0].actions
-                        ? (
-                            row.insights.data[0].actions.find(
-                              (element) => element.action_type === 'like'
-                            ) || {}
-                          ).value
-                        : (row.objective === 'OUTCOME_LEADS' ||
-                            row.objective === 'LEAD_GENERATION') &&
-                          row.insights &&
-                          row.insights.data &&
-                          row.insights.data[0].actions
-                        ? (
-                            row.insights.data[0].actions.find(
-                              (element) => element.action_type === 'lead'
-                            ) || {}
-                          ).value
-                        : (row.objective === 'LINK_CLICKS' ||
-                            row.objective === 'OUTCOME_TRAFFIC') &&
-                          row.insights &&
-                          row.insights.data &&
-                          row.insights.data[0].actions
-                        ? (
-                            row.insights.data[0].actions.find(
-                              (element) => element.action_type === 'link_click'
-                            ) || {}
-                          ).value
-                        : 1
-                    )
-                  ).toFixed(2)}
-                </TableCell>
-                <TableCell align="left">
-                  <span className="status" style={statusStyle(row.status)}>
-                    {row.status}
-                  </span>
-                </TableCell>
-                <TableCell align="center">
-                  {parseInt(
-                    row.insights ? row.insights.data[0].reach : 0
-                  ).toLocaleString('en-US')}
-                </TableCell>
-                <TableCell align="center">
-                  {parseInt(
-                    row.insights ? row.insights.data[0].impressions : 0
-                  ).toLocaleString('en-US')}
-                </TableCell>
-                <TableCell align="center">
-                  {parseInt(
-                    row.insights ? row.insights.data[0].clicks : 0
-                  ).toLocaleString('en-US')}
-                </TableCell>
-                <TableCell align="center">
-                  {parseFloat(
-                    row.insights ? row.insights.data[0].cpc : 0
-                  ).toFixed(2)}
-                </TableCell>
-                <TableCell align="center">
-                  {parseFloat(
-                    row.insights ? row.insights.data[0].ctr : 0
-                  ).toFixed(2)}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell align="left" style={{ fontWeight: 'bold' }}>
-                Grand Total
-              </TableCell>
-              <TableCell align="center"></TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
+                            element?.ID ===
+                            parseInt(
+                              contact.properties.facilitador_compra_contacto
+                            )
+                        )?.Nombre || ''}
+                      </td>
+                      <td>{contact.properties.hs_analytics_source}</td>
+                      <td>{contact.properties.lifecyclestage}</td>
+                      <td>{contact.properties.hs_lead_status}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cerrar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </div>
   );
 }
--- client/src/data/google.js
+++ client/src/data/google.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 export const googleCampaignsData = async (since, until) => {
   const response = await axios.get(
-    // `/api/account-insights?since=${since}&until=${until}`
-    '/api/google-campaigns'
+    `https://google-ads-api.onrender.com/api/google-campaigns?since=${since}&until=${until}`
   );
   return response.data;
 };
